11416e95d47aac40e89c91ef3adfe797f6d82976
LeonardoScheffernunes <lsnunes1@minha.fag.edu.br>
2025-11-27 22:46:02 -0300

    Commit 2 - aplicado refatoriamento 1

--- src/Rental.java
+++ src/Rental.java
@@ -1,18 +1,26 @@
 public class Rental {
+    private Movie _movie;
+    private int _daysRented;
 
-   private Movie _movie;
-   private int _daysRented;
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
 
-   public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-   }
+    public int getDaysRented() {
+        return _daysRented;
+    }
 
-   public int getDaysRented() {
-      return _daysRented;
-   }
+    public Movie getMovie() {
+        return _movie;
+    }
 
-   public Movie getMovie() {
-      return _movie;
-   }
+    // EXTRAÍDO DO CUSTOMER - agora pertence ao Rental
+    public double getCharge() {
+        return _movie.getCharge(_daysRented);
+    }
+
+    public int getFrequentRenterPoints() {
+        return _movie.getFrequentRenterPoints(_daysRented);
+    }
 }
