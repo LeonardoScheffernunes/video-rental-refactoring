78f29fda9b025fe9ec91ddb68bfa336dde05e3a1
LeonardoScheffernunes <lsnunes1@minha.fag.edu.br>
2025-11-27 23:31:04 -0300

    commit 15

--- src/Price.java
+++ src/Price.java
@@ -2,9 +2,8 @@ public abstract class Price {
     public abstract int getPriceCode();
     public abstract double getCharge(int daysRented);
 
-    // Passo 6: mover cálculo de pontos frequentes
+    // método genérico para pontos frequentes
     public int getFrequentRenterPoints(int daysRented) {
-        // implementação padrão: 1 ponto
         return 1;
     }
 }
