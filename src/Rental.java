10ab35f4ca396afebe88fc16a1b7877fe933bd97
LeonardoScheffernunes <lsnunes1@minha.fag.edu.br>
2025-11-27 23:11:31 -0300

    commit 9

--- src/Rental.java
+++ src/Rental.java
@@ -1,4 +1,5 @@
 public class Rental {
+
     private Movie _movie;
     private int _daysRented;
 
@@ -15,36 +16,16 @@ public class Rental {
         return _movie;
     }
 
-    // EXTRAÍDO DO CUSTOMER — agora pertence ao Rental
+    // Refactoring 7 – Passo 1: agora apenas delega a Movie
     public double getCharge() {
-        double result = 0;
-
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
-    // NOVO MÉTODO — Refactoring 5
+    // Refactoring anterior: pontos de fidelidade
     public int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            getDaysRented() > 1) {
+        if ((_movie.getPriceCode() == Movie.NEW_RELEASE) && (_daysRented > 1))
             return 2;
-        }
-        return 1;
+        else
+            return 1;
     }
 }
